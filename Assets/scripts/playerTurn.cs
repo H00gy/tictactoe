cca9731eaa94b3df06f028954f1488dc8d903b51
H00gy <148908869+H00gy@users.noreply.github.com>
2026-04-16 15:26:52 -0400

    some button logic and testing for styles

--- Assets/scripts/playerTurn.cs
+++ Assets/scripts/playerTurn.cs
@@ -1,17 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerTurn : MonoBehaviour
 {
-    public Sprite[] XOs;
-    SpriteRenderer sr;
+    public StyleGroup[] styleGroups;
+    public Button[] gridButtons;
+    int buttonIndex;
+    public int styleIndex;
 
     private void Awake()
     {
-        sr = GetComponent<SpriteRenderer>();
+        for(int i = 0; i < gridButtons.Length; i++)
+        {
+            buttonIndex = i;
+            gridButtons[i].image.sprite = null;
+        }
 
     }
     public void onClick()
     {
+        gridButtons[buttonIndex].image.sprite = styleGroups[styleIndex].X;
+    }
 
+
+    // debugging
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            styleIndex = 1;
+            Debug.Log("index is 1");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            styleIndex = 2;
+            Debug.Log("index is 2");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            styleIndex = 3;
+            Debug.Log("index is 3");
+        }
     }
 }
