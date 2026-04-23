aeba6297e282811d5c941eaf30d6240d2909bab5
H00gy <148908869+H00gy@users.noreply.github.com>
2026-04-23 19:32:39 -0400

    settings button

--- Assets/scripts/UIAndHUD/PlaySceneUI/PlayUIManager.cs
+++ Assets/scripts/UIAndHUD/PlaySceneUI/PlayUIManager.cs
@@ -4,10 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class PlayUIManager : MonoBehaviour
 {
-    [Header("Script References")]
+    
     [Header("Popup References")]
     public GameObject ThemeSelect;
     public GameObject StatsPopup;
+    [Header("Button Sound")]
+    public AudioSource PrimaryButtonClickSound;
+    public AudioSource SecondaryButtonClickSound;
 
     void Start()
     {
@@ -18,19 +21,23 @@ public class PlayUIManager : MonoBehaviour
     // play Button Popup
     public void OpenThemeSelect()
     {
+        PrimaryButtonClickSound.Play();
         ThemeSelect.SetActive(true);
     }
     public void CloseThemeSelect()
     {
+        SecondaryButtonClickSound.Play();
         ThemeSelect.SetActive(false);
     }
     public void SelectTheme(int ThemeID)
     {
+        SecondaryButtonClickSound.Play();
         PlayerTurn.StyleIndex = ThemeID;
 
     }
     public void LoadGameScene()
     {
+        PrimaryButtonClickSound.Play();
         SceneManager.LoadScene(0);
     }
 
@@ -38,13 +45,14 @@ public class PlayUIManager : MonoBehaviour
 
     public void OpenStats()
     {
-       
+        PrimaryButtonClickSound.Play();
         StatsPopup.SetActive(true);
         StatsPopup.GetComponent<StatsPopup>().loadStats();
         
     }
     public void CloseStats()
     {
+        SecondaryButtonClickSound.Play();
         StatsPopup.SetActive(false);
     }
 
--- Assets/scripts/UIAndHUD/SettingsButtonScript.cs
+++ Assets/scripts/UIAndHUD/SettingsButtonScript.cs
@@ -3,9 +3,56 @@ using UnityEngine.UI;
 
 public class SettingsButtonScript : MonoBehaviour
 {
+    [Header("Popup Reference")]
+    public GameObject SettingsPopup;
+    [Header("Sound References")]
+    public AudioSource Music;
+    public GameObject SFX;
 
-    public void OnButtonClick()
+
+    private void Start()
     {
+        SettingsPopup.SetActive(false);
+    }
+    public void OpenSettings()
+    {
+        SettingsPopup.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        SettingsPopup.SetActive(false);
+    }
+    public void ToggleMusic(bool isMusicOn)
+    {
+        if (isMusicOn)
+        {
+            Music.Play();
+        }
+        else
+        {
+            Music.Stop();
+        }
         
     }
+    
+    public void ToggleSFX(bool isSFXOn)
+    {
+        if (isSFXOn)
+        {
+            foreach (Transform Sound in SFX.transform)
+            {
+                Sound.GetComponent<AudioSource>().volume = 1;
+            }
+        }
+        else
+        {
+            foreach (Transform Sound in SFX.transform)
+            {
+                Sound.GetComponent<AudioSource>().volume = 0;
+            }
+        }
+        
+        
+    }
+    
 }
